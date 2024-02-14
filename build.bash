17b5d5469151716860150b772ad93fd59a9cff5a
Shiny380 <daniel.mckinnell43@gmail.com>
2024-02-14 13:15:18 +1100

    update point schema endian

--- build.bash
+++ build.bash
@@ -7,5 +7,5 @@ docker build -t module-builder -f Dockerfile.module --build-arg="GITHUB_TOKEN=$G
 docker run -d --name module-builder module-builder:latest
 docker container cp module-builder:/app/$MODULE_NAME .
 docker rm -f module-builder
-docker exec $BIOS_CONTAINER sh -c 'rm -rf /data/rubix-os/data/modules/'$MODULE_NAME'/v0.0.0/; mkdir -p /data/rubix-os/data/modules/'$MODULE_NAME'/v0.0.0/'
-docker cp ./$MODULE_NAME $BIOS_CONTAINER:/data/rubix-os/data/modules/$MODULE_NAME/v0.0.0/
+#docker exec $BIOS_CONTAINER sh -c 'rm -rf /data/rubix-os/data/modules/'$MODULE_NAME'/v0.0.0/; mkdir -p /data/rubix-os/data/modules/'$MODULE_NAME'/v0.0.0/'
+#docker cp ./$MODULE_NAME $BIOS_CONTAINER:/data/rubix-os/data/modules/$MODULE_NAME/v0.0.0/
